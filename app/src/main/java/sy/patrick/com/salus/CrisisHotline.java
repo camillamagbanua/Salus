2a093faa0adbbd83285abcc9f1e8db53bfa90fc3
camillamagbanua <2015084974@ust-ics.mygbiz.com>
2018-12-09 23:53:19 +0800

    crisis hotlines done

--- app/src/main/java/sy/patrick/com/salus/CrisisHotline.java
+++ app/src/main/java/sy/patrick/com/salus/CrisisHotline.java
@@ -1,7 +1,10 @@
 package sy.patrick.com.salus;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CrisisHotline extends AppCompatActivity {
 
@@ -10,5 +13,41 @@ public class CrisisHotline extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crisis_hotline);
         setTitle("Crisis Hotline");
+    }
+
+    public void process(View v) {
+        Intent i = null, chooser = null;
+
+        if (v.getId() == R.id.call911) {
+            i = new Intent(Intent.ACTION_DIAL);
+            i.setData(Uri.parse("tel:911"));
+            startActivity(i);
+        }
+
+        else if (v.getId() == R.id.callbtn1) {
+            i = new Intent(Intent.ACTION_DIAL);
+            i.setData(Uri.parse("tel:028969191"));
+            startActivity(i);
+        }
+
+        else if (v.getId() == R.id.callbtn2) {
+            i = new Intent(Intent.ACTION_DIAL);
+            i.setData(Uri.parse("tel:09178549191"));
+            startActivity(i);
+        }
+
+        else if (v.getId() == R.id.callbtn3) {
+            i = new Intent(Intent.ACTION_DIAL);
+            i.setData(Uri.parse("tel:028044673"));
+            startActivity(i);
+        }
+
+        else if (v.getId() == R.id.callbtn4) {
+            i = new Intent(Intent.ACTION_DIAL);
+            i.setData(Uri.parse("tel:09175584673"));
+            startActivity(i);
+        }
+
+
     }
 }
